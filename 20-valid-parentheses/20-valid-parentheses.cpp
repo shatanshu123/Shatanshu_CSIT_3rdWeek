4f24427e1a6a11927c84ff34b061ab5145d85ad3
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-07-21 22:04:44 +0530

    Time: 6 ms (13.39%), Space: 6.4 MB (10.10%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+bool isValid(string s) {
+
+    stack<char> st;
+    int n = s.size();
+    
+    for(int i=0; i<n; i++)
+    {
+        char ch = s[i];
+        
+        
+        if(ch == '(' || ch == '{' || ch == '[')
+        {
+            st.push(ch);
+        }
+        else
+        {
+            
+            if(!st.empty())
+            {
+                char top = st.top();
+                if( (ch == ')' && top == '(' ) ||
+                   ( ch == '}' && top == '{' ) ||
+                   ( ch == ']' && top == '[' ))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    
+    if(st.empty())
+        return true;
+    else
+        return false;
+}
+};
+
